9535394f159a7eb40890b63677d0ff50813532db
Jayne Gale <jaynegale@gmail.com>
2019-08-28 15:32:25 +1200

    Got receptacles working
    But should be based on item Im carrying script on the Player not the UI

--- Assets/Scripts/Redevelopment/Activatable/Receptacle.cs
+++ Assets/Scripts/Redevelopment/Activatable/Receptacle.cs
@@ -1,23 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Receptacle : MonoBehaviour, IActivatable
 {
-    public GameObject objectIAccept;
-
+    public GameObject objectToGoInReceptacle; //the (inactive )object that will appear and animate into the receptacle
+    public Sprite spriteToolIAccept; //what sprite(s) represents the object(s) I need to activate (that the player has picked up previously and hence has in their hand) eg crystal_green or key_red
+    public GameObject toolPanel; //the panel that that sprite is being shown on 
+    Image toolImage;
+    Sprite toolImageSprite;
     public bool verbose;
 
     void Start()
     {
-        objectIAccept.SetActive(false);
+        objectToGoInReceptacle.SetActive(false);
+        toolImage = toolPanel.GetComponent<Image>();
+        toolImageSprite = toolImage.sprite;
     }
 
     public void Activate()
     {
         if (verbose) print("Activate Method in Receptacle Class starts " + gameObject.name);
-
-        objectIAccept.SetActive(true);
+        if (!toolImage.enabled)
+        {
+            toolImageSprite = null;
+        }
+        else
+        {
+            toolImageSprite = toolImage.sprite;
+        }
+        if (toolImageSprite != null && toolImageSprite == spriteToolIAccept)
+        {
+            objectToGoInReceptacle.SetActive(true);
+            toolImage.enabled = false;
+        }
     }
-
 }
