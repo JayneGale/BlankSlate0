10ffa7ab2ae5d465c4146f9943046cfb3a0084a0
Jayne Gale <jaynegale@gmail.com>
2019-08-30 16:55:48 +1200

    Added Black button textures, tried to make crystal activate button and link buttons

--- Assets/Scripts/Redevelopment/Activatable/ChangeMaterial.cs
+++ Assets/Scripts/Redevelopment/Activatable/ChangeMaterial.cs
@@ -18,62 +18,62 @@ public class ChangeMaterial : MonoBehaviour, IActivatable
     public bool buttonStartsEnabled = true;
 
     bool defaultState = true;
-    public GameObject decideItem;
-    bool decider;
+    //public GameObject decideItem;
+    //bool decider;
 
     private void Start()
     {
         rend = GetComponent<Renderer>();
+        //if (buttonStartsEnabled)
+        //{
+            //if (decideItem.GetComponent<DecideToUnlock>() == null)
+            //{
+            //    print("Deciding item on " + gameObject + "is either not set or has no DecideToUnlock script on it ");
+            //    decider = true;
+            //}
+            //else decider = decideItem.GetComponent<DecideToUnlock>().enableButton;
+
         if (!buttonStartsEnabled)
         {
-            if (decideItem.GetComponent<DecideToUnlock>() == null)
-            {
-                print("Deciding item on " + gameObject + "is either not set or has no DecideToUnlock script on it ");
-                decider = true;
-            }
-            else decider = decideItem.GetComponent<DecideToUnlock>().enableButton;
-
-            if (!decider)
-            {
-                rend.material = disabledMat;
-            }
+            rend.material = disabledMat;
         }
+        //}
         else
         {
             rend.material = state0Mat;
-            decider = true;
+            //decider = true;
         }
         if (verbose) print("Material on renderer is " + rend.material + "and defaultState is " + defaultState);
     }
 
     public void Activate()
     {
-        if (!buttonStartsEnabled)
+        //if (!buttonStartsEnabled)
+        //{
+        //    decider = decideItem.GetComponent<DecideToUnlock>().enableButton;
+        //    print("Decider is " + decider);
+        //}
+        //decider = true;
+        //if (decider)
+        //{
+        rend = GetComponent<Renderer>();
+        if (verbose) print("Material on renderer is " + rend.material + "and defaultState is " + defaultState);
+        if (defaultState)
         {
-            decider = decideItem.GetComponent<DecideToUnlock>().enableButton;
-            print("Decider is " + decider);
-        }
-
-        if (decider)
-        {
-            rend = GetComponent<Renderer>();
+            rend.material = state1Mat;
             if (verbose) print("Material on renderer is " + rend.material + "and defaultState is " + defaultState);
-            if (defaultState)
-            {
-                rend.material = state1Mat;
-                if (verbose) print("Material on renderer is " + rend.material + "and defaultState is " + defaultState);
-            }
-            else
-            {
-                rend.material = state0Mat;
-                if (verbose) print("Material on renderer is " + rend.material + "and defaultState is " + defaultState);
-            }
-            defaultState = !defaultState;
         }
         else
         {
+            rend.material = state0Mat;
             if (verbose) print("Material on renderer is " + rend.material + "and defaultState is " + defaultState);
-            rend.material = disabledMat;
         }
+        defaultState = !defaultState;
+        //}
+        //else
+        //{
+        //    if (verbose) print("Material on renderer is " + rend.material + "and defaultState is " + defaultState);
+        //    rend.material = disabledMat;
+        //}
     }
 }
--- Assets/Scripts/Redevelopment/DecideToUnlock.cs
+++ Assets/Scripts/Redevelopment/DecideToUnlock.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class DecideToUnlock : MonoBehaviour, IActivatable
 {
     public bool enableButton = false;
+    public GameObject ButtonToEnable;
     public bool verbose;
 
     public void Activate()
     {
         if (verbose) print("HiddenItem " + gameObject.name + "enabling the Button now");
+
         enableButton = true;
+        ButtonToEnable.GetComponent<ChangeMaterial>().Activate();
     }
 }
