1a7d9c371d9d4e94904c45d17ce20eddaa4570df
Jayne <jaynegale@gmail.com>
2019-09-03 12:57:35 +1200

    Button Enabler working

---
+++ Assets/Scripts/Redevelopment/ButtonEnabler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEnabler : MonoBehaviour, IActivatable
+{
+    public GameObject ButtonISetActive; //could be an array for more than one target, or add two scripts for two buttons
+    public GameObject ButtonISetInactive; //could be an array for more than one target, or add two scripts for two buttons
+    public bool targetStartsEnabled = true; //belts and braces: buttons default to enabled, on Start turn off the target button if this is set false;
+    public bool verbose;
+
+    private void Start()
+    {
+        if (targetStartsEnabled == true) SetButtonsActive();
+        if (verbose) print("Target Button is " + gameObject.name);
+    }
+
+    public void Activate()
+    {
+        if (verbose) print("DisabledButton " + gameObject.name + "enabling the Button " + gameObject.name);
+        SetButtonsActive();
+    }
+
+    private void SetButtonsActive()
+    {
+        ButtonISetActive.SetActive(true);
+        ButtonISetInactive.SetActive(false);
+    }
+}
--- Assets/Scripts/Redevelopment/DecideToUnlock.cs
+++ Assets/Scripts/Redevelopment/DecideToUnlock.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class DecideToUnlock : MonoBehaviour, IActivatable
 {
     public bool enableButton = false;
-    public GameObject ButtonToEnable;
+    public GameObject DisabledButtonToEnable;
     public bool verbose;
 
     public void Activate()
     {
-        if (verbose) print("HiddenItem " + gameObject.name + "enabling the Button now");
-
         enableButton = true;
-        ButtonToEnable.GetComponent<ChangeMaterial>().Activate();
+        if (verbose) print("DeciderItem " + gameObject.name + "enabling the Button now " + enableButton);
     }
 }
