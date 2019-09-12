3ac7e181d0f7ad60389351c86e877a195dd2a2be
Jayne Gale <jaynegale@gmail.com>
2019-09-12 15:36:31 +1200

    Main Menu scene

---
+++ Assets/Scripts/Redevelopment/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    int thisSceneBuildIndex;
+
+    private void Start()
+    {
+        thisSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void PlayGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        SceneManager.LoadScene(thisSceneBuildIndex + 1);
+    }
+
+    public void ReallyQuit()
+    {
+        Application.Quit();
+    }
+
+}
