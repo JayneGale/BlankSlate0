54ce3915a3afec04bd19023987e09bc7467cf78e
Jayne Gale <jaynegale@gmail.com>
2019-09-26 21:08:13 +1200

    Changing docking material to match socket

---
+++ Assets/DockingCrystalMaterials.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DockingCrystalMaterials : MonoBehaviour
+{
+    public Material[] matColours;
+    Material mat;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    matColours = new Material[7];
+    //}
+
+    public Material SetMaterial(Takeable.Colour colour)
+    {
+        if (colour == Takeable.Colour.red) mat = matColours[0];
+        if (colour == Takeable.Colour.orange) mat = matColours[1];
+        if (colour == Takeable.Colour.yellow) mat = matColours[2];
+        if (colour == Takeable.Colour.green) mat = matColours[3];
+        if (colour == Takeable.Colour.blue) mat = matColours[4];
+        if (colour == Takeable.Colour.indigo) mat = matColours[5];
+        if (colour == Takeable.Colour.violet) mat = matColours[6];
+        return mat;
+    }
+}
--- Assets/Scripts/Redevelopment/Activatable/Receptacle.cs
+++ Assets/Scripts/Redevelopment/Activatable/Receptacle.cs
@@ -10,16 +10,16 @@ public class Receptacle : MonoBehaviour, IActivatable
     public GameObject objectToActiveIfCorrect;
     public Takeable.Colour colourIAccept; //what is the colour of the object(s) I need to activate (that the player has picked up and is carrying)
     public Takeable.Item itemIAccept; // do I need a key or a crystal?
-
     public bool verbose;
     public bool receptacleFull = false;
+    Material mat;
 
     void Start()
     {
         if (verbose) print("Start Method in Receptacle Class starts " + gameObject + colourIAccept + itemIAccept);
-
         if (objectToGoInReceptacle != null)
         {
+            objectToGoInReceptacle.GetComponent<Renderer>().material = objectToGoInReceptacle.GetComponent<DockingCrystalMaterials>().SetMaterial(colourIAccept);            
             objectToGoInReceptacle.SetActive(false);// set inactive the crystal that's ready to drop into the socket
         }
         else print("This receptacle " + gameObject + "has no animation object ready to dock ");
@@ -31,8 +31,6 @@ public class Receptacle : MonoBehaviour, IActivatable
         if (verbose) print("Activate Method, Receptacle Class starts this gameObject accepts colour and item type " + gameObject + colourIAccept + itemIAccept);
         var objsCarried = GameObject.Find("Player").GetComponent<CarryItems>();
         if(objsCarried.HasItem(itemIAccept, colourIAccept) && !receptacleFull)
-            //if (verbose) print("The Item colour the player is carrying is " + objsCarried.colour + " and Item type " + objsCarried.item + " player has an item "+ GameObject.Find("Player").GetComponent<CarryItems>().hasItem);
-            //if (objsCarried.hasItem && colourIAccept == objsCarried.colour && itemIAccept == objsCarried.item && !receptacleFull)
         {
             GoInSocket();
             objsCarried.DropItem(itemIAccept, colourIAccept); // removes this item from list of items player is carrying 
