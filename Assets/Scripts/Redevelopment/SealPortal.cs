3ebcd9a148755fa2f81de49557b984eb39f4b624
Jayne <jaynegale@gmail.com>
2019-10-22 13:57:16 +1300

    Portal seals itself now

---
+++ Assets/Scripts/Redevelopment/SealPortal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SealPortal : MonoBehaviour, IActivatable
+{
+    [SerializeField]
+    private GameObject[] Targets;//for Portal 2 this is Portal 1, and its TWO buttons; for Portal 1 this is Portal 2 and its ONE button
+    public GameObject otherPortal;
+    public bool verbose;
+
+    private Animator Anim;
+    bool otherIsOpening;
+
+    private void Start()
+    {
+        otherIsOpening = otherPortal.GetComponent<Animator>().GetBool("isOpening");
+    }
+
+    public void Activate()
+    {
+        otherIsOpening = otherPortal.GetComponent<Animator>().GetBool("isOpening");
+        if (verbose) print("Before Activate the other portal door is open " + otherIsOpening);
+        if (otherIsOpening)
+        {
+            foreach (var target in Targets)
+            {
+                if (verbose) print("Portal exit triggered activate on " + target.name);
+                foreach (var activatable in target.GetComponents<IActivatable>())
+                {
+                    activatable.Activate();
+                }
+            }
+        }
+        if (verbose) print("After Activate the other portal door is open " + otherIsOpening);
+
+    }
+}
