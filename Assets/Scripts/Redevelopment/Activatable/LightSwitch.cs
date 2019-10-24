c9a2decbe8f46546e5b1656d0b32f1a679442b13
Jayne Gale <jaynegale@gmail.com>
2019-10-24 15:30:41 +1300

    ColumnLight switch not dropzone

---
+++ Assets/Scripts/Redevelopment/Activatable/LightSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour, IActivatable
+{
+    public GameObject LightToSwitch;
+    public bool isOn;
+
+    public void Activate()
+    {
+        LightToSwitch.SetActive(isOn);
+        isOn = !isOn;
+    }
+}
