fdcfcb2e1c6e311f1a6f236dcf15483d902bacf2
Jayne <jaynegale@gmail.com>
2019-10-30 10:16:25 +1300

    Enum for PlayerPos

--- Assets/Scripts/Redevelopment/PortalExitClose.cs
+++ Assets/Scripts/Redevelopment/PortalExitClose.cs
@@ -7,11 +7,35 @@ public class PortalExitClose : MonoBehaviour
     [SerializeField]
     private GameObject[] Targets;
     public bool verbose = false;
+    PlayerPos Location;
+
+    public enum PlayerPos
+
+    {
+        Portal,
+        Room,
+        Room180,
+        ERROR
+    }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            if (gameObject.CompareTag("Portal"))
+            {
+                Location = PlayerPos.Portal;
+            }
+            if (gameObject.CompareTag("Room"))
+            {
+                Location = PlayerPos.Room;
+            }
+            if (gameObject.CompareTag("Room180"))
+            {
+                Location = PlayerPos.Room180;
+            }
+            else Location = PlayerPos.ERROR;
+
             foreach (var target in Targets)
             {
                 if (verbose) print("Portal exit triggered activate on " + target.name);
