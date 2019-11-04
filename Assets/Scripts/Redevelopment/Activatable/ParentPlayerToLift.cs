0087cc693b25005abce6ad331fc4cee6bcd99db4
Jayne <jaynegale@gmail.com>
2019-11-05 12:51:41 +1300

    Lift parenting working on entry only

---
+++ Assets/Scripts/Redevelopment/Activatable/ParentPlayerToLift.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentPlayerToLift : MonoBehaviour, IActivatable
+{
+    public Transform liftShaft;
+    public Transform liftCar;
+    GameObject player;
+    public bool verbose;
+    public float playerRotate;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    public void Activate()
+    {
+        player.transform.SetParent(liftCar, false);
+        //player.transform.parent = liftCar;
+        //if (verbose) print("Player parented to lift car");
+        //player.transform.rotation = Quaternion.identity;
+        //if (verbose) print("Player Quaternion identity");
+        //player.transform.rotation = Quaternion.Euler(0, playerRotate, 0);
+        //if (verbose) print("Player rotated by playerRotate" + playerRotate);
+
+        ////Quaternion deltaRotation = liftShaft.transform.rotation * Quaternion.Inverse(liftCar.transform.rotation);
+        //player.transform.rotation = deltaRotation * player.transform.rotation;
+
+    }
+}
