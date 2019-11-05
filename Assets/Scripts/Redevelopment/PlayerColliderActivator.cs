f01cb06975521f059ae8338a38e351e11d0a4d5d
Jayne <jaynegale@gmail.com>
2019-11-05 15:27:57 +1300

    Broke everything

---
+++ Assets/Scripts/Redevelopment/PlayerColliderActivator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColliderActivator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] Targets;
+    public bool verbose = false;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            foreach (var target in Targets)
+            {
+                if (verbose) print("Portal exit triggered activate on " + target.name);
+                foreach (var activatable in target.GetComponents<IActivatable>())
+                {
+                    activatable.Activate();
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Redevelopment/PortalExitClose.cs
+++ Assets/Scripts/Redevelopment/PortalExitClose.cs
@@ -7,14 +7,11 @@ public class PortalExitClose : MonoBehaviour
     [SerializeField]
     private GameObject[] Targets;
     public bool verbose = false;
-    //public PlayerInZone.PlayerPos ThisZone;
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //PlayerInZone.PlayerPos Location = ThisZone;
-
             foreach (var target in Targets)
             {
                 if (verbose) print("Portal exit triggered activate on " + target.name);
