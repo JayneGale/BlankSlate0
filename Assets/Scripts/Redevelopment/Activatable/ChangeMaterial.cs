e171c99c4245efe7de30034c273912e26b46f8a2
Jayne <jaynegale@gmail.com>
2019-11-19 14:09:14 +1300

    Lift doors, button mats

--- Assets/Scripts/Redevelopment/Activatable/ChangeMaterial.cs
+++ Assets/Scripts/Redevelopment/Activatable/ChangeMaterial.cs
@@ -7,11 +7,18 @@ public class ChangeMaterial : MonoBehaviour, IActivatable
     [Header("Button Materials")]
 
     [SerializeField]
-    private Material mat1; //Green when Portal shut
+    private Material mat1; //Green when Door is shut, ready to open
     [SerializeField]
-    private Material mat2; //Black when Portal open  //Save red for when button is disabled
+    private Material mat2; //Black when Door is open, ready to close  //Save red for when button is disabled
 
-    public bool isMat1 = true; //start material is State0
+    //public bool isMat1 = true; //start material is State0
+    [SerializeField]
+    GameObject doorIOpen;
+    [SerializeField]
+    private string BooleanName;
+
+    Animator anim;
+    public bool isMat1 = true; //isMat1 = mat1 if true, else mat2 if false
 
     Renderer rend;
     public bool verbose;
@@ -19,14 +26,22 @@ public class ChangeMaterial : MonoBehaviour, IActivatable
     private void Start()
     {
         rend = GetComponent<Renderer>();
-        if (isMat1) rend.material = mat1;
-        else rend.material = mat2;
+        if (doorIOpen != null)
+        {
+            anim = doorIOpen.GetComponent<Animator>();
+            if (anim != null) isMat1 = !anim.GetBool(BooleanName);
+            if (verbose) print("isMat1" + isMat1);
+        }
+        rend.material = (isMat1) ? mat1 : mat2 ;
+        //if (isMat1) rend.material = mat1;
+        //else rend.material = mat2;
         if (verbose) print("Material on renderer is " + rend.material + "and isMat1 is " + isMat1);
     }
 
     public void Activate()
     {
         rend = GetComponent<Renderer>();
+        if (anim != null) isMat1 = anim.GetBool(BooleanName); //ie door is open, so should be mat2)
         if (isMat1) rend.material = mat2;
         else rend.material = mat1;
         isMat1 = !isMat1;
