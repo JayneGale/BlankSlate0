4e2590ba845416cd3eb157cc7982cf533ad3f48a
Jayne Gale <jaynegale@gmail.com>
2019-12-01 16:33:02 +1300

    More nope

--- Assets/Scripts/Redevelopment/PlayerInLift.cs
+++ Assets/Scripts/Redevelopment/PlayerInLift.cs
@@ -8,7 +8,7 @@ public class PlayerInLift : MonoBehaviour
     public Transform liftCar; 
     public bool playerInLiftTrigger = false;
     public bool verbose = true;
-    Transform startPlayerTransform;
+    //Quaternion startPlayerRotation;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,9 +16,9 @@ public class PlayerInLift : MonoBehaviour
         {
             playerInLiftTrigger = true;
             if (verbose) print("PlayerInLift.cs player is inside lift " + playerInLiftTrigger);
-            startPlayerTransform = other.GetComponent<Transform>();//store the player's original rotation
+            //startPlayerRotation = other.transform.rotation;//store the player's original rotation
             other.transform.SetParent(liftCar, true);
-            other.transform.rotation = startPlayerTransform.rotation;//set player's rotation back to their original rotation
+            //other.transform.rotation = startPlayerRotation;//set player's rotation back to their original rotation
         }
     }
 
@@ -28,9 +28,9 @@ public class PlayerInLift : MonoBehaviour
         {
             playerInLiftTrigger = false;
             if (verbose) print("PlayerInLift.cs player is inside lift " + playerInLiftTrigger);
-            other.transform.parent = null;
-            //other.transform.SetParent(null, true);
-            other.transform.rotation = Quaternion.identity;
+            //other.transform.parent = null;
+            other.transform.SetParent(null, true);
+            //other.transform.rotation = Quaternion.identity;
         }
     }
 }
